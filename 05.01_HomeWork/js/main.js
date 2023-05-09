a075725fee4beced24b4c42f0e044ea728b6523b
Mendy053 <morian770@gmail.com>
2023-05-09 12:12:54 +0300

    05.03_HomeWork_1

--- 05.01_HomeWork/js/main.js
+++
@@ -1,9 +0,0 @@
-let students = [];
-
-for (let i = 0; i < 5; i++) {
-	students.push(prompt("What is the name of student number " + (i + 1) + "?"));
-}
-
-students = students.sort();
-
-alert("The students list:\n" + students.join(", "));
