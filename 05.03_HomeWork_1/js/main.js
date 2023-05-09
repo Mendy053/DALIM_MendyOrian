3fda02171196628b9d2c0bc27721218a52f6fb43
Mendy053 <morian770@gmail.com>
2023-05-09 12:13:30 +0300

    05.03_HomeWork_1

---
+++ 05.03_HomeWork_1/js/main.js
@@ -0,0 +1,29 @@
+function startProgram(userStr) {
+    printMassage(orderLetters(userStr));
+}
+function orderLetters(userStr) {
+    const VOWELS_LIST = ["a", "e", "i", "o", "u"];
+    const VowelsInString = [];
+    const ConsonantsInString = [];
+
+    for (let i = 0; i < userStr.length; i++) {
+        let = letter = userStr[i].toLowerCase();
+
+        if (VOWELS_LIST.includes(letter)) {
+            VowelsInString.push(letter);
+        } else {
+            ConsonantsInString.push(letter);
+        }
+    }
+    return [
+        VowelsInString.sort().join(", "),
+        ConsonantsInString.sort().join(", ")
+    ];
+}
+
+function printMassage(array = []) {
+    alert(`Your string contains the following vowels: ${array[0]}.
+And the following consonant: ${array[1]}.`);
+}
+
+startProgram(prompt("Please enter your string:"));
